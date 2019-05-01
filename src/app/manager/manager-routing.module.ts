f1cf1fa29894dbe1c30a3f5a513472e2687f83c0
Oz Ruffinelli <ozkrpy@gmail.com>
2019-05-01 17:16:16 -0400

    manager routing module

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ManagerHomeComponent } from './manager-home/manager-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ManagerComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/manager/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: ManagerHomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ManagerRoutingModule { }
+export class ManagerRoutingModule {}
